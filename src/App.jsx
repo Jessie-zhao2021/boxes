4323df8284519ab5c21fc503774697704f630fcc
lovekwe <jessiezhao1816@gmail.com>
2023-03-15 14:48:29 +1100

    add new func

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,26 @@ function App() {
   const [squares, setSquares] = useState(boxes)
   function toggle(id){
     setSquares(prevSquares => {
-      const newSquares = []
-      for(let i = 0; i < prevSquares.length; i++) {
-          const currentSquare = prevSquares[i]
-          if(currentSquare.id === id) {
-              const updatedSquare = {
-                  ...currentSquare,
-                  on: !currentSquare.on
-              }
-              newSquares.push(updatedSquare)
-          } else {
-              newSquares.push(currentSquare)
-          }
-      }
-      return newSquares
+      return prevSquares.map((square) => {
+          return square.id === id ? {...square, on: !square.on} : square
+      })
   })
+  //   setSquares(prevSquares => {
+  //     const newSquares = []
+  //     for(let i = 0; i < prevSquares.length; i++) {
+  //         const currentSquare = prevSquares[i]
+  //         if(currentSquare.id === id) {
+  //             const updatedSquare = {
+  //                 ...currentSquare,
+  //                 on: !currentSquare.on
+  //             }
+  //             newSquares.push(updatedSquare)
+  //         } else {
+  //             newSquares.push(currentSquare)
+  //         }
+  //     }
+  //     return newSquares
+  // })
 }
 
   
